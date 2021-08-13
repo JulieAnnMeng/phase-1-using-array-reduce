8be0b48df57ad5853e5519e536aac9e4cb1f8578
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-12 21:11:49 -0400

    commit tree e3365f8d25644e7e1df05d7c837189e4bba7fcc8

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const reducerF = function(accumulator, currentValue){return accumulator + currentValue}
+
+const totalBatteries = batteryBatches.reduce(reducerF);
+
+
+
+//const batteryFunction = (accumulator, currentValue) => batteryBatches.reduce{return const totalBatteries = (accumulator + currentValue)};
+
+// const totalBatteries = (accumulator, currentValue) => parseInt(accumulator + currentValue)
+// console.log(batteryBatches.reduce(totalBatteries))
